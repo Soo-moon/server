3601e0dbd2242b92d6e5637e18e23a7c2030c1e6
Soo-moon <tkeks96@gmail.com>
2020-02-19 13:23:54 +0900

    q

---
+++ D&M/src/server/serverconn.java
@@ -0,0 +1,66 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class serverconn {
+	static ServerSocket serversocket = null;
+	static Socket clientSocket = null;
+	static DataOutputStream out =null;
+	static DataInputStream in = null;
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		serversocket =new ServerSocket(5000);
+		
+		Thread outThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					out=new DataOutputStream(clientSocket.getOutputStream());
+					String aa = "welcome";
+					out.writeUTF(aa);
+					out.flush();
+					while(true) {
+						System.out.println("타이핑 : ");
+						System.out.println();
+						String t = scan.nextLine();
+						out.writeUTF(t);
+						out.flush();
+						
+						if(t.equals("stop")) {
+							break;
+						}
+					}
+					
+				}catch (Exception e) {
+					// TODO: handle exception
+					System.out.println(e.toString());
+				}
+			}
+		});
+		
+		
+		try {
+			System.out.println("서버생성");
+			clientSocket = serversocket.accept();
+			System.out.println("클라이언트 연결");
+			outThread.start();
+						
+			in = new DataInputStream(clientSocket.getInputStream());
+			System.out.println(in.readUTF());
+
+		}catch (Exception e) {
+			// TODO: handle exception
+			String error = e.toString();
+			System.out.println(error);
+		}
+	}
+
+}
